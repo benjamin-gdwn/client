e46116a0644e78747a87421ea15fe19ca4f0b52d
Ben_G <ben.goodwin.work1@gmail.com>
2023-02-01 16:35:45 +0000

    Channel list containers updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import 'stream-chat-react/dist/css/index.css'
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import { ChannelListContainer, ChannelContainer, Auth } from "./comps";
+import { useState } from "react";
 // storing api key for getstream.io
 // const apiKey = "f8ppdb3652jb";
 
@@ -25,6 +27,11 @@ if (authToken) {
 }
 
 const App = () => {
+
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) {
     return <Auth />;
   }
@@ -32,9 +39,20 @@ const App = () => {
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
-      </Chat>
+                <ChannelListContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                />
+                <ChannelContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
+                />
+            </Chat>
     </div>
   );
 };
